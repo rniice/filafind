c76d6e5e016b56a846b0da04dcf8d04341221a2c
Michael Crockett <michael.crockett@gmail.com>
2016-01-21 01:56:51 -0800

    trying to set port differently

--- server.js
+++ server.js
@@ -7,32 +7,28 @@ var config = require('./js/config.json');
 var materials = require('./js/materials.js');
 
 
-app.listen(config.PORT, function() {        //listen to requests from the local host. 
-	console.log("listening on localhost port: ", config.PORT);
-
-	materials.initialize();					//initialize the materials database
-
-});	
-
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname));
 
 //MAIN landing URL
-app.get('./', function(req, res) {
+app.get('/', function(req, res) {
 	console.log("operating in dirname: " + __dirname);
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 
 //Querying DB for Materials that are Filtered
-app.get('./materials', function(req, res) {
+app.get('/materials', function(req, res) {
 	//res.send('returning all materials');
 	materials.getMaterials(req, res);
 });
 
 //Querying DB for a specific material ID to return properties for
-app.get('./materials/:id', function(req, res) {
+app.get('/materials/:id', function(req, res) {
 	//res.send('returning specific material id');
 	materials.getMaterial(req, res);
 });
@@ -46,9 +42,14 @@ app.get('/materials/find', function(req, res) {
 */
 
 //Adding Material to DB
-app.post('./materials/', function(req, res){
+app.post('/materials/', function(req, res){
 	materials.addMaterial(req.asJSON(), res);
 
 });
 
 
+//starts the server listening
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+    materials.initialize();	
+});
