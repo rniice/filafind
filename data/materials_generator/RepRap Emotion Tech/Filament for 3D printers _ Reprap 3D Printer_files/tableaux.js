28a84e0bf75fb94675fad4e8fdb5479693b314a6
Michael Crockett <michael.crockett@gmail.com>
2016-02-16 12:56:24 -0800

    cleaning up

--- data/materials_generator/RepRap Emotion Tech/Filament for 3D printers _ Reprap 3D Printer_files/tableaux.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	// Tableaux
-	$("table.tablesorter").tablesorter({
-		textExtraction: function(node) {
-			var attr = jQuery(node).attr('rel');
-			return (typeof attr !== 'undefined' && attr !== false) ? attr : jQuery(node).text(); 
-		}
-	});
-});
--- data/materials_generator/RepRap Emotion Tech/test_regex.js
+++
@@ -1,7 +0,0 @@
-var composition_regex = new RegExp('([\\w]+)', 'ig');
-
-var string_test = "dog eat shit bitch";
-
-var result = string_test.match(composition_regex)[0];
-
-console.log(result);
