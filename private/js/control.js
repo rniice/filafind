dc8795545101e2b1a21b72a37ffef3bacfe06207
Michael Crockett <michael.crockett@gmail.com>
2016-03-08 13:45:01 -0800

    using root domain

--- private/js/control.js
+++ private/js/control.js
@@ -2,7 +2,8 @@ var myApp = angular.module('myApp', ['ngTouch','mp.colorPicker', 'rzModule', 'ui
 
 //var base_query = "http://localhost:8080/materials";
 //var base_query = "https://stark-tundra-90514.herokuapp.com/materials";
-var base_query = "http://stark-tundra-90514.herokuapp.com/materials";
+//var base_query = "http://stark-tundra-90514.herokuapp.com/materials";
+var base_query = "https://filafind.com/materials";
 
 var filtered_query = "";      
 
